00be56fb254769d02bf21e1547fdfdb36d77ee46
arkenabd <60082789+arkenabd@users.noreply.github.com>
2020-02-27 14:16:32 +0700

    update

--- src/main/java/com/json/netty/route/ServerRoute.java
+++ src/main/java/com/json/netty/route/ServerRoute.java
@@ -22,7 +22,7 @@ public class ServerRoute extends RouteBuilder {
 		JAXBContext jaxbContext = JAXBContext.newInstance(Ops.class);
 		JaxbDataFormat eventDataFormat = new JaxbDataFormat(jaxbContext);
 
-		restConfiguration().apiContextPath("/openapi.json").component("restlet").contextPath("/hanabank").port("7070")
+		restConfiguration().apiContextPath("/openapi.json").component("restlet").contextPath("/hanabank").port("4040")
 				.scheme("http").apiProperty("api.title", "Hana Bank - Registration").apiProperty("api.version", "1")
 				.apiProperty("api.specification.contentType.json", "application/vnd.oai.openapi+json;version=2.0")
 				.apiProperty("api.specification.contentType.yaml", "application/vnd.oai.openapi;version2.0");
@@ -30,19 +30,13 @@ public class ServerRoute extends RouteBuilder {
 		rest().path("/hello").get().route().transform().constant("Hello World").marshal().json(JsonLibrary.Gson,
 				ResponseType.class);
 
-		rest().path("/reg").post().type(JsonReq.class).to("direct:appCreated");
+		rest().path("/reg").post().consumes("application/json")
+		.type(JsonReq.class).to("direct:appCreated");
 
 		from("direct:appCreated")
-				// unmarshall for mapping parameter input into a POJO
-				//.unmarshal().json(JsonLibrary.Gson, JsonReq.class)
 				.log("Body : (${body})")
-				.log("Body getBasic: (${body.getBasic})")
-				.log("Body getRef : (${body.getBasic.getRef})")
-				.log("Body getTid : (${body.getBasic.getTid})")
-				.log("Body getRemark : (${body.getBasic.getRemark})")
-				.log("Body getOps : (${body.getOps})")
+				.bean("generateFixLengthReq", "generate(${body})")
 				// assign value to a POJO
-				.bean("generateFixLengthReq", "generate(${body.getBasic.getRemark},${body.getOps})")
 				.log("Body after generateFixLengthReq : (${body})")
 				.bean("tcpClient", "call(${body})")
 				//.bean("postBean", "setValue(${body.getId},${body.getName})")
