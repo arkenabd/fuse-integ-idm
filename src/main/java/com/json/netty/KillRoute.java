1fef30f56c93076d1ea942d3b30fe6c48cb3d182
arkenabd <60082789+arkenabd@users.noreply.github.com>
2020-05-27 11:33:42 +0700

    update

--- src/main/java/com/json/netty/KillRoute.java
+++ src/main/java/com/json/netty/KillRoute.java
@@ -7,27 +7,16 @@ import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
 import org.apache.camel.ShutdownRunningTask;
 import org.apache.camel.management.event.ExchangeFailureHandlingEvent;
+import org.apache.camel.spi.ShutdownStrategy;
 
 public class KillRoute implements Processor {
 
 	@Override
 	public void process(Exchange exchange) throws Exception {
-		String id = exchange.getFromRouteId();
-		System.out.println("routeId to be killed:" + id);
-		if (exchange.getContext().getRoute(id) != null) {
-			System.out.println("Start Gracefully removing route " + id);
-			try {
-//				exchange.getContext().getInflightRepository().remove(exchange);
-				exchange.getContext().setShutdownRunningTask(ShutdownRunningTask.CompleteCurrentTaskOnly);
-				/*
-				 * exchange.getContext().stopRoute(id, 1, TimeUnit.MILLISECONDS); //
-				 * exchange.getContext().removeRoute(id); exchange.getContext().startRoute(id);
-				 */
-				System.out.println("End Gracefully removing route " + id);
-			} catch (Exception e) {
-				System.out.println("Failed to remove route: " + id + "," + e.getMessage());
-			}
-		}
+		ShutdownStrategy shutdownStrategy = exchange.getContext().getShutdownStrategy();
+		shutdownStrategy.setTimeout(1000);
+		shutdownStrategy.setTimeUnit(TimeUnit.MILLISECONDS);
+		shutdownStrategy.setShutdownNowOnTimeout(true);
 
 	}
 
