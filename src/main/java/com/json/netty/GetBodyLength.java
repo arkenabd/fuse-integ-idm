2db7986d95f49c6b58e8a979dfc3ca017be407d6
arkenabd <60082789+arkenabd@users.noreply.github.com>
2020-07-13 17:05:39 +0700

    update

--- src/main/java/com/json/netty/GetBodyLength.java
+++ src/main/java/com/json/netty/GetBodyLength.java
@@ -1,5 +1,7 @@
 package com.json.netty;
 
+import java.nio.charset.StandardCharsets;
+
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
 
@@ -8,7 +10,17 @@ public class GetBodyLength implements Processor {
 	@Override
 	public void process(Exchange exchange) throws Exception {
 		// TODO Auto-generated method stub
-		String body = (String) exchange.getIn().getBody();
+		String body = "";
+		try {
+			body = (String) exchange.getIn().getBody().toString();
+			System.out.println("Response ORI from HLI :" + body);
+			if (body.length() != 189 && body.length() != 74) {
+				exchange.getIn().setBody("ERROR");
+			}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		int bodyLength = body.length();
 		bodyLength = bodyLength;
 		exchange.setProperty("bodyLength", bodyLength);
